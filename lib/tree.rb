6679d51cd7820ac99ec9e7e0b830727874ba3e13
Khai Nguyen <khaivnguyen02@gmail.com>
2024-08-06 15:17:57 +0700

    Implement #insert method which accepts a value to insert

--- lib/tree.rb
+++ lib/tree.rb
@@ -20,9 +20,27 @@ class Tree
     root
   end
 
+  def insert(value)
+    @root = insert_recursive(@root, value)
+  end
+
   def pretty_print(node = @root, prefix = '', is_left = true)
     pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
     puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
     pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
+  end
+
+  private
+
+  def insert_recursive(node, value)
+    return Node.new(value) if node.nil?
+
+    if value < node.data
+      node.left = insert_recursive(node.left, value)
+    elsif value > node.data
+      node.right = insert_recursive(node.right, value)
+    end
+
+    node
   end
 end
